c47803d3c625dce217977480bc93cd559089cbee
lokisf8 <matveyd05@yandex.ru>
2025-04-11 21:42:54 +0300

    User model 2 with email and fix issues

--- core/models/transaction.py
+++ core/models/transaction.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class Transaction(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("categorys.id"))
     datetime: Mapped[DateTime] = mapped_column(DateTime)
     amount: Mapped[int] = mapped_column(Integer)
