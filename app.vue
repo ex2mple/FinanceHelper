d4679f5b6de26fcecd452cc2b4b675e3440b6d4f
sl1ki0 <oiltanker9@gmail.com>
2025-04-11 07:44:04 +0300

    Nuxt setup + PrimeVue + Tailwind. Настроено окружение

---
+++ app.vue
@@ -0,0 +1,3 @@
+<template>
+  <Button class="w-1/2 h-1/2">Hey!</Button>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import Aura from '@primeuix/themes/aura';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  css: [
+    'primeicons/primeicons.css', // Add this line
+    // Optionally include the PrimeVue base styles if needed:
+    // 'primevue/resources/primevue.min.css'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+})
